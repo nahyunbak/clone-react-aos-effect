8346e4dd013cca092b87294e2c57d9be9c79e9d6
nahyunbak <nahyunbak@naver.com>
2021-10-11 03:06:17 +0900

    First Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,25 @@
 import logo from './logo.svg';
 import './App.css';
+import Aos from 'aos';
+import "aos/dist/aos.css";
+import { useEffect } from 'react';
+
 
 function App() {
+  useEffect(()=>{
+    Aos.init({duration: 2000});
+
+  }, [])
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1>pricing</h1>
+     <div className="grids">
+     <div className="boxes">1</div>
+     <div className="boxes">2</div>
+     <div data-aos="fade-up" className="boxes">3</div>
+     <div data-aos="zoom-in" className="boxes">4</div>
+     <div data-aos="flip-up" className="boxes">5</div>
+     </div>
     </div>
   );
 }
